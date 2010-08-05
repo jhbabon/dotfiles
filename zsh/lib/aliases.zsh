bfcc7fb354338ac127f152e40f15137c9ea80e07
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2010-08-05 18:57:51 +0200

    Added aliases to control postgresql

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -10,4 +10,8 @@ alias mdp="mkdir -p"
 alias ping='ping -c 4 '
 alias pingg='ping www.google.com'
 
+# postgresql control
+alias pg1='pg_ctl -D /usr/local/var/postgres start -l /usr/local/var/postgres/server.log'
+alias pg0='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
 # vim:set ft=zsh:
