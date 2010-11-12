39e66ec7fa1477cb3f2f6c22dc7341db781eb948
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2010-11-12 12:33:51 +0100

    [FIXBUG] Fixed syntax error in zsh/lib/bindkeys.zsh
    Added some functionality to the history search of zsh.

--- zsh/lib/bindkeys.zsh
+++ zsh/lib/bindkeys.zsh
@@ -4,6 +4,8 @@
 bindkey -v
 
 # use incremental search
-bindkey ^R history-incremental-search-backward
+bindkey "^R" history-incremental-search-backward
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
 
 # vim:set ft=zsh:
