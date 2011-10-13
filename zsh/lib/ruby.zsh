9110c1429eaccd459e622ba4709294f7c450d259
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2011-10-13 21:31:40 +0200

    RUBY: Added rbenv to manage ruby versions. Has preference over RVM.

---
+++ zsh/lib/ruby.zsh
@@ -0,0 +1,14 @@
+#! /bin/zsh
+
+# Ruby version manager: rbenv or rvm, but not both
+
+## rbenv: has preference over rvm
+if [[ -s $HOME/.rbenv ]] ; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+## rvm
+elif [[ -s $HOME/.rvm/scripts/rvm ]] ; then
+  source $HOME/.rvm/scripts/rvm
+fi
+
+# vim:set ft=zsh:
--- zshenv
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-# rvm
-if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
-
-# vim:set ft=zsh:
