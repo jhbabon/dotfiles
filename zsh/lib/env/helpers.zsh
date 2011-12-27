1753ce4f3abb5d480437af2e94791450ef1d3c16
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2011-12-27 19:41:32 +0100

    ZSH: Added some functions to manage an kill processes

--- zsh/lib/env/helpers.zsh
+++ zsh/lib/env/helpers.zsh
@@ -19,4 +19,31 @@ any() {
   fi
 }
 
+
+# fp:
+#   * description: find and list processes matching a
+#                  case-insensitive partial-match string
+#   * link: http://brettterpstra.com/fk-redux/
+fp() {
+  ps Ao pid,comm |\
+    awk '{match($0,/[^\/]+$/); print substr($0,RSTART,RLENGTH)": "$1}'|\
+    grep -i $1|\
+    grep -v grep
+}
+
+# fk:
+#   * description: kill the selected process matching the given param
+#   * link: http://brettterpstra.com/fk-redux/
+fk() {
+  IFS=$'\n'
+  PS3='Kill which process? (1 to cancel): '
+  select OPT in "Cancel" $(fp $1); do
+    if [ $OPT != "Cancel" ]; then
+      kill $(echo $OPT|awk '{print $NF}')
+    fi
+    break
+  done
+  unset IFS
+}
+
 # vim:set ft=zsh:
