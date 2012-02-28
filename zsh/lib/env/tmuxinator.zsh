9643254eb95231462ff1babec093189a030fe0ed
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2012-02-28 22:48:36 +0100

    ZSH: Deleted rake completion. Added tmuxinator loading

---
+++ zsh/lib/env/tmuxinator.zsh
@@ -0,0 +1,6 @@
+# tmuxinator gem: to manage tmux sessions
+# @link: https://github.com/aziz/tmuxinator
+
+[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
+
+# vim:set ft=zsh:
