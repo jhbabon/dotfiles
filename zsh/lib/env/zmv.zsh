2daa7364f2533ef232bdc024df822552774e67ce
Juan Hernández <juan.hernandez.babon@gmail.com>
2012-08-13 10:49:09 +0300

    ZSH: Add an example to zmv lib.

--- zsh/lib/env/zmv.zsh
+++ zsh/lib/env/zmv.zsh
@@ -1,5 +1,8 @@
 # zmv: zsh renaming tool
 # @link: http://www.mfasold.net/blog/2008/11/moving-or-renaming-multiple-files/
+#
+# Example:
+#   % mmv '(*).json' '$1.rb'
 
 autoload -U zmv
 alias mmv='noglob zmv -w'
