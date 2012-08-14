8718ea61d57914e9d99e9fa57bff2d475776777b
Juan Hernández <juan.hernandez.babon@gmail.com>
2012-08-14 19:22:01 +0300

    ZSH: Share history between sessions.
    @link: http://nuclearsquid.com/writings/shared-history-in-zsh/

--- zsh/lib/env/history.zsh
+++ zsh/lib/env/history.zsh
@@ -2,6 +2,10 @@
 
 # ignore duplicate history entries
 setopt histignoredups
+# Appends every command to the history file once it is executed
+setopt inc_append_history
+# Reloads the history whenever you use it
+setopt share_history
 
 # keep more history
 export HISTFILE=$HOME/.zshistory
