f04d9da269cc012d808b9ba0fea40e7ae984f9b3
Juan Hernández <juan.hernandez.babon@gmail.com>
2016-08-10 15:53:43 +0200

    Add new fish_prompt function.
    Show more info about git and use another set of colors.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,69 +1,66 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+function fish_prompt --description "Write out the prompt"
+  set -l last_status $status
 
-	# Just calculate this once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
+  # Hack; fish_config only copies the fish_prompt function (see #736)
+  if not set -q -g __fish_classic_git_functions_defined
+    set -g __fish_classic_git_functions_defined
 
-	set -l normal (set_color normal)
+    function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
 
-	# Hack; fish_config only copies the fish_prompt function (see #736)
-	if not set -q -g __fish_classic_git_functions_defined
-		set -g __fish_classic_git_functions_defined
+    function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
 
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-		
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-		
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
+    # initialize global variables
+    if not set -q __my_git_prompt_initialized
+      set -qU fish_color_cwd; or set -U fish_color_cwd blue
+      set -qU fish_color_status; or set -U fish_color_status red
 
-		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
+      set -qU __fish_git_prompt_showdirtystate; or set -U __fish_git_prompt_showdirtystate "yes"
+      set -qU __fish_git_prompt_showstashstate; or set -U __fish_git_prompt_showstashstate "yes"
+      set -qU __fish_git_prompt_showuntrackedfiles; or set -U __fish_git_prompt_showuntrackedfiles "yes"
+      set -qU __fish_git_prompt_showupstream; or set -U __fish_git_prompt_showupstream "auto informative name git"
+      set -qU __fish_git_prompt_showcolorhints; or set -U __fish_git_prompt_showcolorhints "yes"
+      set -qU __fish_git_prompt_show_informative_status; or set -U __fish_git_prompt_show_informative_status ""
+      set -qU __fish_git_prompt_char_dirtystate; or set -U __fish_git_prompt_char_dirtystate "!"
+      set -qU __fish_git_prompt_char_invalidstate; or set -U __fish_git_prompt_char_invalidstate "*"
+      set -qU __fish_git_prompt_char_untrackedfiles; or set -U __fish_git_prompt_char_untrackedfiles "?"
 
-		# initialize our new variables
-		if not set -q __fish_classic_git_prompt_initialized
-			set -qU fish_color_user; or set -U fish_color_user -o green
-			set -qU fish_color_host; or set -U fish_color_host -o cyan
-			set -qU fish_color_status; or set -U fish_color_status red
-			set -U __fish_classic_git_prompt_initialized
-		end
-	end
+      set -qU fish_color_status; or set -U fish_color_status red
+      set -U __my_git_prompt_initialized
+    end
+  end
 
-	set -l color_cwd
-	set -l prefix
-	switch $USER
-	case root toor
-		if set -q fish_color_cwd_root
-			set color_cwd $fish_color_cwd_root
-		else
-			set color_cwd $fish_color_cwd
-		end
-		set suffix '#'
-	case '*'
-		set color_cwd $fish_color_cwd
-		set suffix '>'
-	end
+  set -l suffix
+  set -l color_cwd
+  set -l color_status (set_color $fish_color_status)
+  set -l normal (set_color normal)
+  set -l prompt_status
+  set -l git_sha_color (set_color yellow)
 
-	set -l prompt_status
-	if test $last_status -ne 0
-		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
-	end
+  switch $USER
+  case root toor
+    if set -q fish_color_cwd_root
+      set color_cwd (set_color $fish_color_cwd_root)
+    end
+    set suffix " # "
+  case "*"
+    set color_cwd (set_color $fish_color_cwd)
+    set suffix " > "
+  end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
+  if test $last_status -ne 0
+    set prompt_status " " "$color_status" "($last_status)" "$normal"
+  end
+
+  set -l git_sha (command git rev-parse --short HEAD ^/dev/null)
+  set -l git_format (printf " $color_cwd($normal%s %s$color_cwd)$normal" "$git_sha_color$git_sha$normal" "%s")
+
+  echo -n -s $color_cwd (prompt_pwd) $normal (__fish_git_prompt $git_format) $normal $prompt_status $color_cwd $suffix $normal
 end
