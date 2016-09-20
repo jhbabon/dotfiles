001cf9506719a33309185ef3123838cc3e82dd1a
Juan Hernández <juan.hernandez.babon@gmail.com>
2016-09-20 11:05:20 -0400

    FISH: Add slate aliases.

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -6,3 +6,10 @@ alias bi "bundle install"
 alias be "bundle exec"
 
 alias ... "cd ../../"
+
+alias sls "slate set"
+alias slg "slate get"
+alias sle "slate exec"
+alias sll "slate list"
+alias slr "slate remove"
+alias slm "slate rename"
