a07cf953ba24918de7a5e79de1e367a6c43c15d2
Juan Hernández <juan.hernandez.babon@gmail.com>
2016-10-24 11:55:37 +0200

    FZF: Configure fzf to use ripgrep

--- fish/conf.d/exports.fish
+++ fish/conf.d/exports.fish
@@ -2,6 +2,5 @@ set -x QT_AUTO_SCREEN_SCALE_FACTOR 2
 set -x EDITOR vim
 set -x VISUAL vim
 
-set -x PATH $HOME/.local/bin $PATH
+set -x PATH $HOME/.local/bin /usr/local/sbin $PATH
 
-set -x FZF_DEFAULT_COMMAND 'ag -g ""'
