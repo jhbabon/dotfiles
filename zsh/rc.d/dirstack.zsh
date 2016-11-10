eab68f3b5717e81143ee9bf773aaffb08033630b
Juan Hernández <juan.hernandez.babon@gmail.com>
2016-11-10 10:45:30 +0100

    ZSH: Don't autocd to last dirstack entry.

--- zsh/rc.d/dirstack.zsh
+++ zsh/rc.d/dirstack.zsh
@@ -4,7 +4,6 @@
 DIRSTACKFILE="$HOME/.cache/zsh/dirs"
 if [[ -f $DIRSTACKFILE ]] && [[ $#dirstack -eq 0 ]]; then
   dirstack=( ${(f)"$(< $DIRSTACKFILE)"} )
-  [[ -d $dirstack[1] ]] && cd $dirstack[1]
 fi
 
 chpwd() {
