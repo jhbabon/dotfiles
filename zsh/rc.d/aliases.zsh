98a8ca01da18329d80e5648037f535eacfaeb88e
Juan Hernández <juan.hernandez.babon@gmail.com>
2017-01-28 11:50:46 +0100

    ZSH: Add an alias to start a private session
    This session will not save any command to the global ZSH history.

--- zsh/rc.d/aliases.zsh
+++ zsh/rc.d/aliases.zsh
@@ -16,6 +16,9 @@ alias hs='fc -l 1'
 # search command in history
 # @link: http://viget.com/extend/level-up-your-shell-game#history-expansions
 alias 'h?'='history | grep'
+# don't save current history in the global history file.
+# history will be lost once the session is closed.
+alias hide='fc -pa' 
 
 # find the option for using colors in ls, depending on the version: Linux or BSD
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
