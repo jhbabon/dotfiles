0f1bc1a1e2ef49b8f927dc37d6611fac9c4865c2
Juan Hernández <juan.hernandez.babon@gmail.com>
2018-02-12 11:19:21 +0000

    NEOVIM: Simplify installation process

--- neovim/neovim.mk
+++ neovim/neovim.mk
@@ -4,21 +4,17 @@ ifdef NEOVIM
 INSTALLERS += neovim
 CLEANERS   += clean_neovim
 
-NEOVIM_SRC_DIR  := $(DOTFILES)/neovim
+NEOVIM_SRC_DIR  := $(DOTFILES)/neovim/nvim
 NEOVIM_DST_DIR  := $(CONFIG_DIR)/nvim
-NEOVIM_INIT     := $(NEOVIM_DST_DIR)/init.vim
 NEOVIM_PLUG_URL := https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 NEOVIM_PLUG     := $(NEOVIM_DST_DIR)/autoload/plug.vim
 
 .PHONY: neovim clean_neovim
 
-neovim: banner_install_neovim $(NEOVIM_INIT) $(NEOVIM_PLUG)
-
-$(NEOVIM_INIT): $(NEOVIM_DST_DIR)
-	$(LINK) $(NEOVIM_SRC_DIR)/init.vim $@
+neovim: banner_install_neovim $(NEOVIM_DST_DIR) $(NEOVIM_PLUG)
 
 $(NEOVIM_DST_DIR):
-	$(LINK) $(NEOVIM_SRC_DIR)/nvim $@
+	$(LINK) $(NEOVIM_SRC_DIR) $@
 
 $(NEOVIM_PLUG): $(NEOVIM_DST_DIR)
 	$(MKDIR) $(@D)
@@ -26,7 +22,7 @@ $(NEOVIM_PLUG): $(NEOVIM_DST_DIR)
 	nvim +PlugInstall +qa
 
 clean_neovim: banner_clean_neovim
-	$(RM) $(NEOVIM_INIT)
+	$(RM) $(NEOVIM_PLUG)
 	$(RM) $(NEOVIM_DST_DIR)
 
 endif
