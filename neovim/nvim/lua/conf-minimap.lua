eeddef8eb255d8a4f96883d1cf940fb53309630d
Juan Hernández Babón <jhbabon@github.com>
2022-03-15 16:44:59 +0100

    OK, let's try this one more time

--- neovim/nvim/lua/conf-minimap.lua
+++
@@ -1,7 +0,0 @@
-return function()
-  vim.g.minimap_close_filetypes = { "startify", "netrw", "vim-plug", "fugitive", "git", "which_key", "lspinfo" }
-
-  local keychain = require("keychain")
-  keychain.nmap("<leader>mt", ":MinimapToggle<cr>", { silent = true, hint = { "minimap", "toggle" } })
-  keychain.nmap("<leader>mr", ":MinimapRefresh<cr>", { silent = true, hint = { "minimap", "toggle" } })
-end
--- neovim/nvim/lua/conf-sidebar.lua
+++
@@ -1,9 +0,0 @@
-return function()
-  require("sidebar-nvim").setup({
-    open = false,
-    sections = { "todos", "git", "diagnostics", "symbols" },
-  })
-
-  local keychain = require("keychain")
-  keychain.nmap("<leader>ds", [[:SidebarNvimToggle<cr>]], { hint = { "diagnostics", "sidebar" } })
-end
