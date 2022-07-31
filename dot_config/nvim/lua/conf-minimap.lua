5193294b6a77cf1bc686da7b31574de33705cd84
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2022-07-31 12:47:43 +0200

    neovim: Fix hint label in minimap config

--- dot_config/nvim/lua/conf-minimap.lua
+++ dot_config/nvim/lua/conf-minimap.lua
@@ -3,5 +3,5 @@ return function()
 
   local keychain = require("keychain")
   keychain.set("n", "<leader>mt", ":MinimapToggle<cr>", { silent = true, hint = { "minimap", "toggle" } })
-  keychain.set("n", "<leader>mr", ":MinimapRefresh<cr>", { silent = true, hint = { "minimap", "toggle" } })
+  keychain.set("n", "<leader>mr", ":MinimapRefresh<cr>", { silent = true, hint = { "minimap", "refresh" } })
 end
