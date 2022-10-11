ab6ba29af2905ea3cc5b5ba9d4c7e7a6dff433e4
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2022-10-11 22:22:53 +0200

    neovim: Cleanup and fix gitsigns keymaps

--- dot_config/nvim/lua/conf-git.lua
+++ dot_config/nvim/lua/conf-git.lua
@@ -1,48 +1,74 @@
 return function()
-	local keychain = require("keychain")
-
 	-- Fugitive
-	keychain.set("n", "<leader>gs", [[:Git<cr>]], { hint = { "git", "status" } })
-	keychain.set("n", "<leader>gc", [[:Git commit<cr>]], { hint = { "git", "commit" } })
-	keychain.set("n", "<leader>gl", [[:Gclog<cr>]], { hint = { "git", "log" } })
-	keychain.set("n", "<leader>gd", [[:Gdiff<cr>]], { hint = { "git", "diff" } })
+	local keys = require("keychain")
+	keys.set("n", "<leader>gs", [[:Git<cr>]], { hint = { "git", "status" } })
+	keys.set("n", "<leader>gc", [[:Git commit<cr>]], { hint = { "git", "commit" } })
+	keys.set("n", "<leader>gl", [[:Gclog<cr>]], { hint = { "git", "log" } })
+	keys.set("n", "<leader>gd", [[:Gdiff<cr>]], { hint = { "git", "diff" } })
 
 	-- Gitsigns
 	-- remove default keymaps
 	require("gitsigns").setup({ keymaps = {} })
+
 	-- add custom keymaps
-	keychain.set(
-		"n",
-		"]c",
-		"&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
-		{ expr = true, hint = { "git", "next hunk" } }
-	)
-	keychain.set(
-		"n",
-		"[c",
-		"&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
-		{ expr = true, hint = { "git", "prev hunk" } }
-	)
+	local group = vim.api.nvim_create_augroup("gitsigns-mappings", { clear = true })
+	vim.api.nvim_create_autocmd("BufEnter", {
+		group = group,
+		callback = function(args)
+			local keychain = require("keychain")
+			local gitsigns = require("gitsigns")
+			local buffer = args.buf
 
-	keychain.set("n", "<leader>hs", [[<cmd>Gitsigns stage_hunk<CR>]], { hint = { "git", "stage hunk" } })
-	keychain.vmap("<leader>hs", [[:Gitsigns stage_hunk<CR>]], { hint = { "git", "stage hunk" } })
-	keychain.set("n", "<leader>hu", [[<cmd>Gitsigns undo_stage_hunk<CR>]], { hint = { "git", "undo stage hunk" } })
-	keychain.set("n", "<leader>hr", [[<cmd>Gitsigns reset_hunk<CR>]], { hint = { "git", "reset hunk" } })
-	keychain.vmap("<leader>hr", [[:Gitsigns reset_hunk<CR>]], { hint = { "git", "reset hunk" } })
-	keychain.set("n", "<leader>hR", [[<cmd>Gitsigns reset_buffer<CR>]], { hint = { "git", "reset buffer" } })
-	keychain.set("n", "<leader>hp", [[<cmd>Gitsigns preview_hunk<CR>]], { hint = { "git", "preview hunk" } })
-	keychain.set("n", "<leader>hb", function()
-		return require("gitsigns").blame_line({ full = true })
-	end, { hint = { "git", "blame" } })
-	keychain.set("n", "<leader>hS", [[<cmd>Gitsigns stage_buffer<CR>]], { hint = { "git", "stage buffer" } })
-	keychain.set(
-		"n",
-		"<leader>hU",
-		[[<cmd>Gitsigns reset_buffer_index<CR>]],
-		{ hint = { "git", "reset buffer index" } }
-	)
+			if not vim.wo.diff then
+				keychain.set("n", "]c", gitsigns.next_hunk, { buffer = buffer, hint = { "git", "next hunk" } })
+				keychain.set("n", "[c", gitsigns.prev_hunk, { buffer = buffer, hint = { "git", "prev hunk" } })
+			end
 
-	-- Text objects
-	keychain.map("o", "ih", [[:<C-U>Gitsigns select_hunk<CR>]], { hint = { "git", "" } })
-	keychain.map("x", "ih", [[:<C-U>Gitsigns select_hunk<CR>]], { hint = { "git", "" } })
+			keychain.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = buffer, hint = { "git", "stage hunk" } })
+			keychain.set("v", "<leader>hs", gitsigns.stage_hunk, { buffer = buffer, hint = { "git", "stage hunk" } })
+			keychain.set(
+				"n",
+				"<leader>hu",
+				gitsigns.undo_stage_hunk,
+				{ buffer = buffer, hint = { "git", "undo stage hunk" } }
+			)
+			keychain.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = buffer, hint = { "git", "reset hunk" } })
+			keychain.set("v", "<leader>hr", gitsigns.reset_hunk, { buffer = buffer, hint = { "git", "reset hunk" } })
+			keychain.set(
+				"n",
+				"<leader>hR",
+				gitsigns.reset_buffer,
+				{ buffer = buffer, hint = { "git", "reset buffer" } }
+			)
+			keychain.set(
+				"n",
+				"<leader>hp",
+				gitsigns.preview_hunk,
+				{ buffer = buffer, hint = { "git", "preview hunk" } }
+			)
+
+			local function blame()
+				return gitsigns.blame_line({ full = true })
+			end
+
+			keychain.set("n", "<leader>hb", blame, { buffer = buffer, hint = { "git", "blame" } })
+
+			keychain.set(
+				"n",
+				"<leader>hS",
+				gitsigns.stage_buffer,
+				{ buffer = buffer, hint = { "git", "stage buffer" } }
+			)
+			keychain.set(
+				"n",
+				"<leader>hU",
+				gitsigns.reset_buffer_index,
+				{ buffer = buffer, hint = { "git", "reset buffer index" } }
+			)
+
+			-- Text objects
+			keychain.map("o", "ih", [[:<C-U>Gitsigns select_hunk<CR>]], { buffer = buffer, hint = { "git", "" } })
+			keychain.map("x", "ih", [[:<C-U>Gitsigns select_hunk<CR>]], { buffer = buffer, hint = { "git", "" } })
+		end,
+	})
 end
