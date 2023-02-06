00742594b640a21e6b411f471d42578d8ba74d5b
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-02-06 22:30:16 +0100

    neovim: Do not add neovim's runtime to lua LSP
    It makes it so slow

--- dot_config/nvim/plugin/lsp/lua-language-server.lua
+++ dot_config/nvim/plugin/lsp/lua-language-server.lua
@@ -25,10 +25,6 @@ local settings = {
 			-- Get the language server to recognize the `vim` global
 			globals = { "vim", "hs" },
 		},
-		workspace = {
-			-- Make the server aware of Neovim runtime files
-			library = vim.api.nvim_get_runtime_file("", true),
-		},
 		-- Do not send telemetry data containing a randomized but unique identifier
 		telemetry = {
 			enable = false,
