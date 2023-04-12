c1fa15d20bc16fad445b6680b151b2a05b28f410
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-04-12 19:18:10 +0200

    neovim: tune up rose-pine italics

--- dot_config/nvim/lua/conf-theme.lua
+++ dot_config/nvim/lua/conf-theme.lua
@@ -1,152 +1,160 @@
 return function()
-	local function statusline(theme)
-		-- Dirbuf customizations
-		local dirbuf = {
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"filename",
-						newfile_status = true,
-						path = 1,
-					},
-				},
-				lualine_x = {
-					{
-						"filetype",
-						icons_enabled = false,
-					},
-				},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			filetypes = { "dirbuf" },
-		}
+  local function statusline(theme)
+    -- Dirbuf customizations
+    local dirbuf = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "filename",
+            newfile_status = true,
+            path = 1,
+          },
+        },
+        lualine_x = {
+          {
+            "filetype",
+            icons_enabled = false,
+          },
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      filetypes = { "dirbuf" },
+    }
 
-		require("lualine").setup({
-			options = {
-				theme = theme,
-				globalstatus = true,
-				disabled_filetypes = {
-					winbar = {
-						"Trouble",
-						"dirbuf",
-						"aerial",
-						"fugitive",
-						"gitcommit",
-						"minimap",
-					},
-				},
-			},
-			extensions = { "aerial", "fugitive", dirbuf },
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff" },
-				lualine_c = { "diagnostics" },
-			},
-			winbar = {
-				lualine_b = {
-					{
-						"filename",
-						newfile_status = true,
-						path = 1,
-					},
-				},
-				lualine_c = { "aerial" },
-			},
-			inactive_winbar = {
-				lualine_b = {
-					{
-						"filename",
-						newfile_status = true,
-						path = 1,
-					},
-				},
-			},
-		})
-	end
+    require("lualine").setup({
+      options = {
+        theme = theme,
+        globalstatus = true,
+        disabled_filetypes = {
+          winbar = {
+            "Trouble",
+            "dirbuf",
+            "aerial",
+            "fugitive",
+            "gitcommit",
+            "minimap",
+          },
+        },
+      },
+      extensions = { "aerial", "fugitive", dirbuf },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "diagnostics" },
+      },
+      winbar = {
+        lualine_b = {
+          {
+            "filename",
+            newfile_status = true,
+            path = 1,
+          },
+        },
+        lualine_c = { "aerial" },
+      },
+      inactive_winbar = {
+        lualine_b = {
+          {
+            "filename",
+            newfile_status = true,
+            path = 1,
+          },
+        },
+      },
+    })
+  end
 
-	-- keep track of the theme names
-	local names = {}
-	-- keep track of their indexes
-	local indexes = {}
-	-- keep track of the current index
-	local current = 1
-	-- use a different key name so the metatable
-	-- proxy methods are called
-	local function key(k)
-		return ("_%s"):format(k)
-	end
+  -- keep track of the theme names
+  local names = {}
+  -- keep track of their indexes
+  local indexes = {}
+  -- keep track of the current index
+  local current = 1
+  -- use a different key name so the metatable
+  -- proxy methods are called
+  local function key(k)
+    return ("_%s"):format(k)
+  end
 
-	local proxy = {
-		__index = function(t, k)
-			current = indexes[k]
-			return rawget(t, key(k))
-		end,
-		__newindex = function(t, k, value)
-			table.insert(names, k)
-			for index, name in ipairs(names) do
-				indexes[name] = index
-			end
+  local proxy = {
+    __index = function(t, k)
+      current = indexes[k]
+      return rawget(t, key(k))
+    end,
+    __newindex = function(t, k, value)
+      table.insert(names, k)
+      for index, name in ipairs(names) do
+        indexes[name] = index
+      end
 
-			rawset(t, key(k), value)
-		end,
-	}
+      rawset(t, key(k), value)
+    end,
+  }
 
-	local themes = {}
-	setmetatable(themes, proxy)
+  local themes = {}
+  setmetatable(themes, proxy)
 
-	-- Cycle through the themes, from current to next
-	local function next_theme()
-		local len = table.maxn(names)
-		local nxt = current + 1
-		if nxt > len then
-			nxt = 1
-		end
-		local name = names[nxt]
+  -- Cycle through the themes, from current to next
+  local function next_theme()
+    local len = table.maxn(names)
+    local nxt = current + 1
+    if nxt > len then
+      nxt = 1
+    end
+    local name = names[nxt]
 
-		themes[name]()
-	end
+    themes[name]()
+  end
 
-	-- NOTE: I'm using the term "palette" because I have already mappings starting with t (theme) or c (colors)
-	require("keychain").set("n", [[<leader>pn]], next_theme, { hint = { "palette", "set next color palette" } })
+  -- NOTE: I'm using the term "palette" because I have already mappings starting with t (theme) or c (colors)
+  require("keychain").set("n", [[<leader>pn]], next_theme, { hint = { "palette", "set next color palette" } })
 
-	-- Define themes
-	function themes.everforest()
-		vim.g.everforest_background = "hard"
-		vim.g.everforest_enable_italic = true
-		vim.g.everforest_better_performance = true
+  -- Define themes
+  function themes.everforest()
+    vim.g.everforest_background = "hard"
+    vim.g.everforest_enable_italic = true
+    vim.g.everforest_better_performance = true
 
-		vim.opt.background = "dark"
-		vim.cmd([[colorscheme everforest]])
+    vim.opt.background = "dark"
+    vim.cmd([[colorscheme everforest]])
 
-		statusline("everforest")
-	end
+    statusline("everforest")
+  end
 
-	function themes.rosepine()
-		require("rose-pine").setup({
-			dark_variant = "moon",
-		})
+  function themes.rosepine()
+    require("rose-pine").setup({
+      dark_variant = "moon",
+      disable_italics = true,
+      highlight_groups = {
+        Comment = { fg = "muted", italic = true },
+        Keyword = { fg = "pine", italic = true },
+        htmlItalic = { italic = true },
+        mkdCode = { fg = "foam", italic = true },
+        ["@text.emphasis"] = { italic = true },
+      },
+    })
 
-		vim.opt.background = "dark"
-		vim.cmd([[colorscheme rose-pine]])
+    vim.opt.background = "dark"
+    vim.cmd([[colorscheme rose-pine]])
 
-		statusline("rose-pine")
-	end
+    statusline("rose-pine")
+  end
 
-	function themes.kanagawa()
-		require("kanagawa").setup({})
+  function themes.kanagawa()
+    require("kanagawa").setup({})
 
-		vim.opt.background = "dark"
-		vim.cmd([[colorscheme kanagawa]])
+    vim.opt.background = "dark"
+    vim.cmd([[colorscheme kanagawa]])
 
-		statusline("kanagawa")
-	end
+    statusline("kanagawa")
+  end
 
-	-- Setup theme
-	local default = "rosepine"
-	local name = vim.env.DOTFILES_NVIM_THEME or default
-	local theme = themes[name] or themes[default]
-	theme()
+  -- Setup theme
+  local default = "rosepine"
+  local name = vim.env.DOTFILES_NVIM_THEME or default
+  local theme = themes[name] or themes[default]
+  theme()
 end
