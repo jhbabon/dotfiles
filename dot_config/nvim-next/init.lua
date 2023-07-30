39addb195cec4e60873584afe9a9137e1fd0d35a
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-07-30 13:39:40 +0200

    neovim: install nvim-treesitter package with chezmoi
    * Configure nvim-treesitter using a regular plugin file
    * Add a bootstrap mechanism with User events. This is executed after
      `chezmoi apply`

--- dot_config/nvim-next/init.lua
+++ dot_config/nvim-next/init.lua
@@ -8,24 +8,24 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 -- Backups
-vim.opt.backup = false   -- no backup
+vim.opt.backup = false -- no backup
 vim.opt.swapfile = false -- no swap files
 
 -- Behaviors
-vim.opt.modeline = true    -- allow modelines in files
-vim.opt.hidden = true      -- keep a buffer even if it's closed
-vim.opt.autoread = true    -- automatically read changes done outside vim
+vim.opt.modeline = true -- allow modelines in files
+vim.opt.hidden = true -- keep a buffer even if it's closed
+vim.opt.autoread = true -- automatically read changes done outside vim
 vim.opt.joinspaces = false -- put only one space after joining
-vim.opt.mouse = "a"        -- enable all mouse interactions
+vim.opt.mouse = "a" -- enable all mouse interactions
 
 -- Use the system clipboard as the default register
 -- TODO: Check https://github.com/ojroques/nvim-osc52 for ssh copy&paste
 vim.opt.clipboard = { "unnamed", "unnamedplus" }
 
 -- Indentation
-vim.opt.tabstop = 2      -- number of visual spaces per TAB
-vim.opt.shiftwidth = 0   -- number of spaces to use for each step of (auto)indent. 0 uses tabstop
-vim.opt.softtabstop = 2  -- number of spaces in tab when editing
+vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.shiftwidth = 0 -- number of spaces to use for each step of (auto)indent. 0 uses tabstop
+vim.opt.softtabstop = 2 -- number of spaces in tab when editing
 vim.opt.shiftround = true
 vim.opt.expandtab = true -- tabs are spaces
 vim.opt.copyindent = false
@@ -44,7 +44,7 @@ vim.opt.listchars = {
 -- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true -- override ignorecase if uppercase is used when searching
-vim.opt.hlsearch = true  -- highlight the search
+vim.opt.hlsearch = true -- highlight the search
 vim.opt.incsearch = true -- search as you type
 
 -- Wildmenu
@@ -54,11 +54,11 @@ vim.opt.wildmode = "list:longest,full"
 -- Position
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.visualbell = true     -- use visual bell, not sound
-vim.opt.shortmess = "aI"      -- modify the error and info messages
-vim.opt.scrolloff = 3         -- screen lines to keep above and below the cursor
+vim.opt.visualbell = true -- use visual bell, not sound
+vim.opt.shortmess = "aI" -- modify the error and info messages
+vim.opt.scrolloff = 3 -- screen lines to keep above and below the cursor
 vim.opt.virtualedit = "block" -- put the cursor anywhere in visual blocks
-vim.opt.cursorline = true     -- show where you are
+vim.opt.cursorline = true -- show where you are
 vim.opt.inccommand = "split"
 
 -- Prevent text jumping with linters/lsp integrations
@@ -102,3 +102,11 @@ vim.keymap.set("n", "<leader>fp", function()
 	vim.fn.setreg("+", vim.fn.expand("%"))
 end, { desc = desc({ "files", "copy path" }) })
 vim.keymap.set("n", "<leader>sc", [[:nohl<cr>]], { desc = desc({ "search", "clear current highlight" }) })
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "Bootstrap",
+	callback = function()
+		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+		ts_update()
+	end,
+})
---
+++ dot_config/nvim-next/plugin/treesitter.lua
@@ -0,0 +1,144 @@
+-- nvim-treesitter configuration
+
+if vim.g.__treesitter_plugin__ then
+	return
+end
+vim.g.__treesitter_plugin__ = true
+
+-- Register a Bootstrap hook
+vim.api.nvim_create_autocmd("User", {
+	pattern = "Bootstrap",
+	callback = function()
+		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+		ts_update()
+	end,
+})
+
+require("nvim-treesitter.configs").setup({
+	-- list from https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+	ensure_installed = {
+		"bash",
+		"c",
+		"c_sharp",
+		"clojure",
+		"cmake",
+		"comment",
+		"commonlisp",
+		"cpp",
+		"css",
+		"dockerfile",
+		"eex",
+		"elixir",
+		"erlang",
+		"fennel",
+		"fish",
+		"glimmer",
+		"go",
+		"gomod",
+		"gowork",
+		"graphql",
+		"hjson",
+		"html",
+		"http",
+		"java",
+		"javascript",
+		"jsdoc",
+		"json",
+		"json5",
+		"jsonc",
+		"julia",
+		"kotlin",
+		"latex",
+		"llvm",
+		"lua",
+		"make",
+		"markdown",
+		"nix",
+		"pascal",
+		"perl",
+		"php",
+		-- "phpdoc",
+		"prisma",
+		"python",
+		"query",
+		"r",
+		"regex",
+		"ruby",
+		"rust",
+		"scala",
+		"scheme",
+		"scss",
+		"svelte",
+		-- "todotxt",
+		"toml",
+		"tsx",
+		"typescript",
+		"vala",
+		"vim",
+		"vimdoc",
+		"vue",
+		"yaml",
+		"zig",
+	},
+	sync_install = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	incremental_selection = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	-- TODO: Add descriptions to mappings
+	textobjects = {
+		enable = true,
+		swap = {
+			enable = true,
+			swap_next = {
+				["<localleader>L"] = "@parameter.inner",
+				["<localleader>J"] = "@function.outer",
+				["<localleader>I"] = "@block.outer",
+			},
+			swap_previous = {
+				["<localleader>H"] = "@parameter.inner",
+				["<localleader>K"] = "@function.outer",
+				["<localleader>U"] = "@block.outer",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["<localleader>l"] = "@parameter.inner",
+				["]f"] = "@function.outer",
+				["]b"] = "@block.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["<localleader><localleader>l"] = "@parameter.inner",
+				["]F"] = "@function.outer",
+				["]B"] = "@block.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["<localleader>h"] = "@parameter.inner",
+				["[f"] = "@function.outer",
+				["[b"] = "@block.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["<localleader><localleader>h"] = "@parameter.inner",
+				["[F"] = "@function.outer",
+				["[B"] = "@block.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+	},
+})
+
+-- Use treesitter's folding module
+vim.opt.foldlevel = 5
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
