6855b2a9d0ea469374279b78abcd79c20bca3471
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-07-31 19:31:25 +0200

    neovim(next): improve neogit configuration
    * Offload setup
    * Add keymaps

--- dot_config/nvim-next/plugin/git.lua
+++ dot_config/nvim-next/plugin/git.lua
@@ -3,6 +3,20 @@ if vim.g.__git_plugin__ then
 end
 vim.g.__git_plugin__ = true
 
-vim.cmd([[packadd neogit]])
+require("offload")(function()
+	vim.cmd([[packadd neogit]])
+	require("neogit").setup({})
 
-require("neogit").setup({})
+	local tools = require("tools")
+
+	local function open()
+		require("neogit").open()
+	end
+
+	local function commit()
+		require("neogit").open({ "commit" })
+	end
+
+	vim.keymap.set("n", "<leader>gs", open, { desc = tools.desc({ "git", "status" }) })
+	vim.keymap.set("n", "<leader>gc", commit, { desc = tools.desc({ "git", "commit" }) })
+end)
