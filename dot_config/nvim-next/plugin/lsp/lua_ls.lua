af823af67f32ef91f204ad4939a3c9dcd382905b
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-08-03 23:24:11 +0200

    neovim(next): fix lua_ls runtime path setting

--- dot_config/nvim-next/plugin/lsp/lua_ls.lua
+++ dot_config/nvim-next/plugin/lsp/lua_ls.lua
@@ -3,6 +3,10 @@ if vim.g.__lsp_lua_ls_plugin__ then
 end
 vim.g.__lsp_lua_ls_plugin__ = true
 
+local path = vim.api.nvim_get_runtime_file("", true)
+table.insert(path, "lua/?.lua")
+table.insert(path, "lua/?/init.lua")
+
 require("lazy-lsp").lua_ls.setup({
 	lazy = {
 		pattern = { "lua" },
@@ -12,14 +16,11 @@ require("lazy-lsp").lua_ls.setup({
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
+				path = path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
 			},
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
