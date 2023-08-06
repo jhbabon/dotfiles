333c81966e3ecbe906805ace16374faf937d60bc
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-08-06 15:30:14 +0200

    neovim(next): install and configure nvim-spectre

---
+++ dot_config/nvim-next/plugin/spectre.lua
@@ -0,0 +1,20 @@
+if vim.g.__spectre_plugin__ then
+	return true
+end
+vim.g.__spectre_plugin__ = true
+
+local defer = require("defer")
+
+defer.offload(function()
+	local function setup()
+		require("spectre").setup()
+	end
+
+	local wrap = defer.jits({ name = "spectre", setup = setup })
+
+	local open = wrap(function()
+		require("spectre").open({})
+	end)
+
+	vim.keymap.set("n", "<leader>sp", open, { desc = _G.desc({ "spectre", "open" }) })
+end)
