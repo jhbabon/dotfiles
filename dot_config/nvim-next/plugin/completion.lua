1ecd20251475ef6e2c244ad1d6e1e38a3bdc35e9
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2023-08-06 13:29:38 +0200

    neovim(next): configure snippy as snippets engine

--- dot_config/nvim-next/plugin/completion.lua
+++ dot_config/nvim-next/plugin/completion.lua
@@ -8,19 +8,6 @@ require("defer").offload(function()
 		lsp_completion = {
 			source_func = "completefunc",
 			auto_setup = false,
-			process_items = function(items, base)
-				local res = vim.tbl_filter(function(item)
-					-- Keep items which match the base and ALSO snippets
-					local word = (item.textEdit and item.textEdit.newText) or item.insertText or item.label or ""
-					return vim.startswith(word, base)
-				end, items)
-
-				table.sort(res, function(a, b)
-					return (a.sortText or a.label) < (b.sortText or b.label)
-				end)
-
-				return res
-			end,
 		},
 	})
 
---
+++ dot_config/nvim-next/plugin/snippets.lua
@@ -0,0 +1,29 @@
+if vim.g.__snippets_plugin__ then
+	return
+end
+vim.g.__snippets_plugin__ = true
+
+require("defer").offload(function()
+	require("snippy").setup({})
+
+	local mappings = require("snippy.mapping")
+
+	vim.keymap.set("i", "<Tab>", mappings.expand_or_advance("<Tab>"))
+	vim.keymap.set("s", "<Tab>", mappings.next("<Tab>"))
+	vim.keymap.set({ "i", "s" }, "<S-Tab>", mappings.previous("<S-Tab>"))
+	vim.keymap.set("x", "<Tab>", mappings.cut_text, { remap = true })
+	vim.keymap.set("n", "g<Tab>", mappings.cut_text, { remap = true })
+
+	-- Snippets menu
+	vim.keymap.set("i", "<c-r><tab>", function()
+		require("snippy").complete()
+	end)
+
+	local group = vim.api.nvim_create_augroup("__snippy_complete__", { clear = true })
+	vim.api.nvim_create_autocmd("CompleteDone", {
+		group = group,
+		callback = function()
+			require("snippy").complete_done()
+		end,
+	})
+end)
