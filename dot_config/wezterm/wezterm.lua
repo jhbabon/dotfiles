7faf00dbdce7c4717d84fa4a418295a90373985d
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2024-03-14 15:55:21 +0100

    wezterm: Always start a new window at 85% screen dimensions and at the center

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -69,4 +69,26 @@ config.font_size = 14.0
 
 config.color_scheme = "Papercolor Light (Gogh)"
 
+wezterm.on("gui-startup", function()
+	local screen = wezterm.gui.screens().active
+	local _, _, window = wezterm.mux.spawn_window({})
+
+	local gui_window = window:gui_window()
+
+	-- Set dimensions to 85% of current screen size
+	-- The actual dimensions will be a bit bigger if we take into
+	-- account the decorations on top
+	local width = screen.width * 0.85
+	local height = screen.height * 0.85
+
+	gui_window:set_inner_size(width, height)
+
+	-- Position the window at the center of the screen
+	local dimensions = gui_window:get_dimensions()
+	local x = (screen.width - dimensions.pixel_width) * 0.5
+	local y = (screen.height - dimensions.pixel_height) * 0.5
+
+	gui_window:set_position(x, y)
+end)
+
 return config
