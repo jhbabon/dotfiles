2bbf1b2017ac05740818816c77b5ee1373487148
Juan Hernández Babón <jhbabon@github.com>
2024-05-23 17:22:30 +0200

    neovim: Install precognition.nvim plugin

---
+++ dot_config/nvim/plugin/precognition.lua
@@ -0,0 +1,28 @@
+---Configure configure precognition plugin: https://github.com/tris203/precognition.nvim
+-- Precognition uses virtual text and gutter signs to show available motions.
+-----------------------------------------------------------------------
+if vim.g.__precognition_plugin__ then
+	return true
+end
+vim.g.__precognition_plugin__ = true
+
+local defer = require("defer")
+
+defer.offload(function()
+	local function setup()
+		vim.cmd([[packadd precognition.nvim]])
+		require("precognition").setup({
+			startVisible = false,
+			showBlankVirtLine = false,
+		})
+	end
+
+	local wrap = defer.jits.precognition(setup)
+
+	local toggle = wrap(function()
+		require("precognition").toggle()
+	end)
+
+	require("clue")("n", "<leader>p", "precognition")
+	vim.keymap.set("n", "<leader>pt", toggle, { desc = "toggle precognition" })
+end)
